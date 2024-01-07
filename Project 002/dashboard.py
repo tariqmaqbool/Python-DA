cf77b09d13431f063ce52ef019de8d631f9d2310
Tariq Maqbool <tariq.maqbool@yahoo.com>
2024-01-07 12:35:15 +0400

    Update Project 2

--- Project 002/dashboard.py
+++ Project 002/dashboard.py
@@ -7,19 +7,53 @@ import warnings
 warnings.filterwarnings('ignore')
 
 #Page Configure 
-st.set_page_config(page_title="H&Y Fashion Sale - Dashboard",page_icon=":shopping_trolley:",layout="wide")
+st.set_page_config(
+    page_title="H&Y Fashion Sale - Dashboard",
+    page_icon=":shopping_trolley:",
+    layout="wide"
+)
 st.title(":shopping_trolley: H&Y Fusion Shop - (Eid Sale Data) Dashboard: Data Analyst using Python")
 st.markdown('<style>div.block-container{padding-top:1rem;}</style>', unsafe_allow_html=True)
 
+
 #load Data
-df = pd.read_csv("sales_data.csv")
-df_col = ['Gender','Age','Marital_Status','State','Occupation','Orders','Amount']
+df = pd.read_csv("sales_data.csv", encoding="ISO-8859-1")
+df_col = ['Gender','Age','Marital_Status','City','Occupation','Orders','Amount']
 
 #lefe menu
 st.sidebar.header("Filter:")
-selected_cities = st.sidebar.selectbox("Select City",df['State'].unique())
-filter_data = df[(df['State'] == selected_cities)]
+selected_cities = st.sidebar.multiselect("Select City",df['City'].unique())
+# occ_list = [''] + list(df['Occupation'].unique())
+
+
+if not selected_cities:
+        filter_data = df
+        cities = None
+else:
+    filter_data = df[df['City'].isin(selected_cities)]
+    cities = filter_data["City"].unique()
+
+
 
 
 #Main Dashboard
-st.table(filter_data[df_col])
+col1,col2 = st.columns((2))  
+#Main Dashboard
+with col1:
+    st.subheader("Orders by Cities")
+    # st.write(f"Dispaly Data Year: {filter_data}")
+    city_df = filter_data.groupby(by = ["City"], as_index = False)["Orders"].sum()
+    fig = px.bar(city_df, x = "City", y="Orders",template = "seaborn")
+    st.plotly_chart(fig,use_container_width=True)
+
+with col2:
+    st.subheader(f"City wise Sale Report:")
+    fig = px.pie(filter_data, values= "Amount", names="City", hole = 0.5)
+    fig.update_traces(text = df["City"], textposition = "outside")
+    st.plotly_chart(fig,use_container_width=True)
+
+
+    # st.subheader(f"Orders by Occupation: {cities}")
+    # fig = px.pie(filter_data, values= "Orders", names="Gender", hole = 0.5)
+    # fig.update_traces(text = df["Gender"], textposition = "outside")
+    # st.plotly_chart(fig,use_container_width=True)
--- README.md
+++ README.md
@@ -2,12 +2,12 @@
 1. <b>Project 001:<b> <br>
    (Data Analyzt of "Gapminder" Data which was available 1952 to 2007. This datasheet contains a wide range of indicators related to global development and analyze trends and patterns in areas such as population, life expectancy, and more.)  using Python with (Streamlit library)<br>
 
-   <b>Dashboard</b>
+   <b>Dashboard</b><br>
    <img src="./images/p1-dashboard.gif" alt="Dashboard">
-   
-   <br><b>Data Sheet</b>
-   <img src="./images/p1-d2.png" alt="Data Sheet">
-   <br><b>Graph</b>
-   <img src="./images/p1-d1.png" alt="Graph">
-   
+
+2. <b>Project 002:</b><br>
+This comprehensive Sales Data Analysis Report provides a detailed examination of sales performance across various key aspects, including gender, city, order, and sale amount. By dissecting these dimensions, the report aims to uncover valuable insights that can inform strategic decision-making and drive targeted business improvements.<br>
+   <b>Dashboard</b><br>
+   <img src="./images/p2-dashboard.gif" alt="Dashboard">
+
 * by: Tariq Maqbool
